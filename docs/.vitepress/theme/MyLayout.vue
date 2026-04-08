f0f0ba67d8fdbb29dfb5c9e46289c15ff7f12259
dmode90 <dmode90@gmail.com>
2026-04-08 07:45:58 +0300

    replace vitepress-component-medium-zoom with medium-zoom and fix z-index

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,13 +1,19 @@
 <script setup>
 import DefaultTheme from "vitepress/theme";
-import { MediumZoom } from "vitepress-component-medium-zoom";
-import "vitepress-component-medium-zoom/style.css";
+import { nextTick, onMounted } from "vue";
+import mediumZoom from "medium-zoom";
+import "./medium-zoom.css";
 
 const { Layout } = DefaultTheme;
+
+onMounted(async () => {
+  await nextTick();
+  mediumZoom(".vp-doc img", {
+    background: "var(--vp-c-bg)",
+  });
+});
 </script>
 
 <template>
-  <Layout>...</Layout>
-  <!-- Use here -->
-  <MediumZoom />
+  <Layout />
 </template>
